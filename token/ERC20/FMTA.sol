746ef72abe2810e7f7b60708245ee82767ce90e7
hooftly <mhooft@tf-dev.org>
2020-09-01 17:08:26 -0600

    Rework some stuff and add Pausing Mechanism

--- token/ERC20/FMTA.sol
+++ token/ERC20/FMTA.sol
@@ -10,6 +10,8 @@ contract FMTAToken is ERC20, Ownable, AccessControl {
     uint256 public _premine;
     bytes32 public constant _MINTER = keccak256("_MINTER");
     bytes32 public constant _BURNER = keccak256("_BURNER");
+    bool public paused;
+    
     
     
     constructor() public {
@@ -19,25 +21,45 @@ contract FMTAToken is ERC20, Ownable, AccessControl {
         _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
     }
     
-    function mint(address _to, uint _amount) public {
+    modifier pause() {
+        require(!paused, "Contract is Paused");
+        _;
+    }
+    
+    function mintTo(address _to, uint _amount) public pause {
         require(hasRole(_MINTER, msg.sender));
         _mint(_to, _amount);
     }
     
-    function burn(uint _amount) public onlyOwner { 
+    function mint( uint _amount) public pause {
+        require(hasRole(_MINTER, msg.sender));
+        _mint(msg.sender, _amount);
+    }
+    
+    function burn(uint _amount) public pause { 
         require(hasRole(_BURNER, msg.sender));
         _burn(msg.sender,  _amount);
     }
+    
+    function burnFrom(address _from, uint _amount) public pause {
+        require(hasRole(_BURNER, msg.sender));
+        _burn(_from, _amount);
+    }
 
-    function cap() public view returns (uint256) {
+    function supplyCap() public view returns (uint256) {
         return _cap;
     }
 
     function _beforeTokenTransfer(address from, address to, uint256 amount) internal virtual override {
         super._beforeTokenTransfer(from, to, amount);
 
-        if (from == address(0)) { // When minting tokens
+        if (from == address(0)) { 
             require(totalSupply().add(amount) <= _cap, "There is a Supply Cap dude. Come on...");
         }
     }
+    
+    function setPaused(bool _paused) public onlyOwner {
+        paused = _paused;
+    }
+    
 }
