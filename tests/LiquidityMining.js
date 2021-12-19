b7bbf347ab67b8cc9922508e21dac0cd5f62fc69
Angry Wasp <angrywasp@live.com>
2021-12-19 11:52:14 +1100

    Fix some tests

--- tests/LiquidityMining.js
+++ tests/LiquidityMining.js
@@ -1,15 +1,16 @@
 var { master, account1, account2 } = require('../lib/accounts.js');
 var helpers = require('../lib/helpers.js');
 var deploy = require('../lib/deployment.js');
+var role = require("../lib/roles.js");
 
 contract('LiquidityMiningTest', () => {
     let liquidityMining, fmtaToken;
     
     before(async function () {
         //Not the real FMTA token
-        fmtaToken = await deploy.token();
+        fmtaToken = await deploy.token(master.address);
 
-        liquidityMining = await deploy.liquidityMining();
+        liquidityMining = await deploy.liquidityMining(master.address);
 
     });
 
@@ -19,12 +20,13 @@ contract('LiquidityMiningTest', () => {
 
         //This token contract can't mint while paused. So unpause it
         await fmtaToken.setPaused(false, { from: master.address });
+        await fmtaToken.disableMintTo(false, { from: master.address });
         
         //mint some tokens to account1
         await fmtaToken.mintTo(account1.address, helpers.toAtomicUnits(10000), { from: master.address });
 
         //get the balance of account1
-        var balance = await deployed.balanceOf(account1.address);
+        var balance = await fmtaToken.balanceOf(account1.address);
 
         //fromAtomicUnits is a BigDecimal. use .value to get a nicely formatted value
         console.log("Balance:", helpers.fromAtomicUnits(balance).value)
